37854f84efc03389b8964f70054d43a4368dd92c
sawertsand22 <85335401+sawertsand22@users.noreply.github.com>
2023-08-18 14:40:36 +0300

    Обновленние до версии  0.2.5
    Changelog 
    - обновлено до версии 0.2
    - Добавлена обработка неверной ссылки при добавлении
    (теперь нельзя ввести неверный url)
    - теперь при добавлении категории она не дублируется
     (раньше при добавлении категории если не выйти из приложения категория просто добавлялась в список даже если она уже была)
    - теперь при нажатии кнопки отмены при добавлении  проиходит отмена:
    -  1)в базу данных не добавляется категория
    -  2)в системе не создается папка
    -  добавил подчищение памяти для запросов
    - было решено перейти с регулярных выражений на libxml для удобства поиска ссылок.
    -  теперь иногда появляются картинки.

---
+++ build-AppParsing-0.2.5/AppParsing_autogen/include/ui_database.h
@@ -0,0 +1,161 @@
+/********************************************************************************
+** Form generated from reading UI file 'database.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DATABASE_H
+#define UI_DATABASE_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_DataBase
+{
+public:
+    QLabel *label;
+    QPushButton *pushButton_2;
+    QWidget *layoutWidget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QWidget *layoutWidget1;
+    QHBoxLayout *horizontalLayout_2;
+    QTextBrowser *textBrowser;
+    QTextBrowser *textBrowser_2;
+    QTextBrowser *textBrowser_3;
+    QComboBox *comboBox_2;
+    QPushButton *pushButton_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QPushButton *pushButton;
+    QComboBox *comboBox;
+
+    void setupUi(QWidget *DataBase)
+    {
+        if (DataBase->objectName().isEmpty())
+            DataBase->setObjectName(QString::fromUtf8("DataBase"));
+        DataBase->resize(801, 521);
+        label = new QLabel(DataBase);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(10, 10, 181, 17));
+        pushButton_2 = new QPushButton(DataBase);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(420, 70, 89, 25));
+        layoutWidget = new QWidget(DataBase);
+        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setGeometry(QRect(0, 140, 801, 20));
+        horizontalLayout = new QHBoxLayout(layoutWidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(layoutWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout->addWidget(label_2);
+
+        label_3 = new QLabel(layoutWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        horizontalLayout->addWidget(label_3);
+
+        label_4 = new QLabel(layoutWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        horizontalLayout->addWidget(label_4);
+
+        layoutWidget1 = new QWidget(DataBase);
+        layoutWidget1->setObjectName(QString::fromUtf8("layoutWidget1"));
+        layoutWidget1->setGeometry(QRect(0, 170, 801, 201));
+        horizontalLayout_2 = new QHBoxLayout(layoutWidget1);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        textBrowser = new QTextBrowser(layoutWidget1);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+
+        horizontalLayout_2->addWidget(textBrowser);
+
+        textBrowser_2 = new QTextBrowser(layoutWidget1);
+        textBrowser_2->setObjectName(QString::fromUtf8("textBrowser_2"));
+
+        horizontalLayout_2->addWidget(textBrowser_2);
+
+        textBrowser_3 = new QTextBrowser(layoutWidget1);
+        textBrowser_3->setObjectName(QString::fromUtf8("textBrowser_3"));
+
+        horizontalLayout_2->addWidget(textBrowser_3);
+
+        comboBox_2 = new QComboBox(DataBase);
+        comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
+        comboBox_2->setGeometry(QRect(0, 410, 191, 25));
+        pushButton_4 = new QPushButton(DataBase);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(200, 410, 89, 25));
+        label_5 = new QLabel(DataBase);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(0, 50, 81, 17));
+        label_6 = new QLabel(DataBase);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setGeometry(QRect(0, 380, 191, 17));
+        label_6->setLayoutDirection(Qt::LeftToRight);
+        label_6->setWordWrap(false);
+        pushButton = new QPushButton(DataBase);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(720, 0, 80, 25));
+        comboBox = new QComboBox(DataBase);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(0, 70, 421, 25));
+
+        retranslateUi(DataBase);
+
+        QMetaObject::connectSlotsByName(DataBase);
+    } // setupUi
+
+    void retranslateUi(QWidget *DataBase)
+    {
+        DataBase->setWindowTitle(QCoreApplication::translate("DataBase", "Form", nullptr));
+        label->setText(QCoreApplication::translate("DataBase", "\320\237\320\276\320\273\321\214\320\267\320\276\320\262\320\260\321\202\320\265\320\273\321\214 : Admin", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("DataBase", "\320\276\321\202\320\272\321\200\321\213\321\202\321\214", nullptr));
+        label_2->setText(QCoreApplication::translate("DataBase", "name", nullptr));
+        label_3->setText(QCoreApplication::translate("DataBase", "idlink", nullptr));
+        label_4->setText(QCoreApplication::translate("DataBase", "datechange", nullptr));
+        textBrowser->setHtml(QCoreApplication::translate("DataBase", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nullptr));
+        textBrowser_2->setHtml(QCoreApplication::translate("DataBase", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nullptr));
+        textBrowser_3->setHtml(QCoreApplication::translate("DataBase", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("DataBase", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214", nullptr));
+        label_5->setText(QCoreApplication::translate("DataBase", "\320\232\320\260\321\202\320\265\320\263\320\276\321\200\320\270\320\270", nullptr));
+        label_6->setText(QCoreApplication::translate("DataBase", "\320\241\321\202\320\260\321\202\321\214\320\270", nullptr));
+        pushButton->setText(QCoreApplication::translate("DataBase", "Add", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class DataBase: public Ui_DataBase {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DATABASE_H
---
+++ build-AppParsing-0.2.5/CMakeFiles/AppParsing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sawert/qtprojects/build-AppParsing-unknown-Debug/AppParsing_autogen/mocs_compilation.cpp" "CMakeFiles/AppParsing.dir/AppParsing_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/AppParsing.dir/AppParsing_autogen/mocs_compilation.cpp.o.d"
+  "/home/sawert/qtprojects/AppParsing/database.cpp" "CMakeFiles/AppParsing.dir/database.cpp.o" "gcc" "CMakeFiles/AppParsing.dir/database.cpp.o.d"
+  "/home/sawert/qtprojects/AppParsing/main.cpp" "CMakeFiles/AppParsing.dir/main.cpp.o" "gcc" "CMakeFiles/AppParsing.dir/main.cpp.o.d"
+  "/home/sawert/qtprojects/AppParsing/mainwindow.cpp" "CMakeFiles/AppParsing.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/AppParsing.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
